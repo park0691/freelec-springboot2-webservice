d4bc44879f8c65194e7589efc165fce566e3444f
park0691 <mem29238@gmail.com>
2020-07-24 17:19:09 +0900

    게시판 템플릿 추가, API 구현
    * 머스테치 템플릿 사용
    * 게시글 CRUD 기능 추가

---
+++ src/main/java/com/practice/book/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,23 @@
+package com.practice.book.springboot.web.dto;
+
+import com.practice.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import javax.persistence.Column;
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+
+}
